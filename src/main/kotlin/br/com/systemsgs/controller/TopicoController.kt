1d3a2163aaf2fc862dd4dba1e1be9ce99d005cfa
GuilhermeJWT <guiromanno@gmail.com>
2022-04-13 14:28:03 -0300

    Utilizando o Bean Validation

--- src/main/kotlin/br/com/systemsgs/controller/TopicoController.kt
+++ src/main/kotlin/br/com/systemsgs/controller/TopicoController.kt
@@ -4,6 +4,7 @@ import br.com.systemsgs.dto.TopicoDTOForm
 import br.com.systemsgs.dto.TopicoView
 import br.com.systemsgs.service.TopicoService
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/topico")
@@ -20,7 +21,7 @@ class TopicoController (private val service: TopicoService){
     }
 
     @PostMapping("/salvar")
-    fun cadastrar(@RequestBody dto: TopicoDTOForm){
+    fun cadastrar(@RequestBody @Valid dto: TopicoDTOForm){
         service.cadastrar(dto)
     }
 
