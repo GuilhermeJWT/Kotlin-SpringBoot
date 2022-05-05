3e697f7d768d88acfafa4a195b14016d130851c1
Guilherme Santos <guiromanno@gmail.com>
2022-05-05 12:56:25 -0300

    Atualiza Topico

--- src/main/kotlin/br/com/systemsgs/dto/AtualizacaoTopicoForm.kt
+++ src/main/kotlin/br/com/systemsgs/dto/AtualizacaoTopicoForm.kt
@@ -3,18 +3,17 @@ package br.com.systemsgs.dto
 import org.jetbrains.annotations.NotNull
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.Size
-import kotlin.math.min
-
 data class AtualizacaoTopicoForm (
+
 
     @field:NotNull
     val id: Long,
 
     @field:NotEmpty
     @field:Size(min = 5, max = 100)
-    val titulo: String,
+    val mensagem: String,
 
     @field:NotEmpty
     @field:Size(min = 5, max = 100)
-    val mensagem: String
+    val titulo: String,
 )
