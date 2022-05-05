08a184be7e8ccf093cf1db6ff08334376f88758f
Guilherme Santos <guiromanno@gmail.com>
2022-05-05 13:57:19 -0300

    Melhorando o Código das respostas Http na APi

--- src/main/kotlin/br/com/systemsgs/controller/TopicoController.kt
+++ src/main/kotlin/br/com/systemsgs/controller/TopicoController.kt
@@ -4,7 +4,10 @@ import br.com.systemsgs.dto.AtualizacaoTopicoForm
 import br.com.systemsgs.dto.TopicoDTOForm
 import br.com.systemsgs.dto.TopicoView
 import br.com.systemsgs.service.TopicoService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
 import javax.validation.Valid
 
 @RestController
@@ -22,16 +25,20 @@ class TopicoController (private val service: TopicoService){
     }
 
     @PutMapping("/atualizar")
-    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm){
-        service.atualizar(form)
+    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm):ResponseEntity<TopicoView>{
+        val topicoView = service.atualizar(form)
+        return ResponseEntity.ok(topicoView)
     }
 
     @PostMapping("/salvar")
-    fun cadastrar(@RequestBody @Valid form: TopicoDTOForm){
-        service.cadastrar(form)
+    fun cadastrar(@RequestBody @Valid form: TopicoDTOForm, uriBuilder: UriComponentsBuilder): ResponseEntity<TopicoView>{
+        val topicoView = service.cadastrar(form)
+        val uri = uriBuilder.path("/topico/pesquisar/${topicoView.id}").build().toUri()
+        return ResponseEntity.created(uri).body(topicoView)
     }
 
     @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deletar(@PathVariable id: Long){
         service.delete(id)
     }
